6ec47974fd41332f8cf370c2881ff45e69027e1b
[pobomb] <patriciaohtani@gmail.com>
2016-05-29 12:35:26 -0500

    Typography with scale and vertical rhythm and added intro content

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'modular-scale'
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
